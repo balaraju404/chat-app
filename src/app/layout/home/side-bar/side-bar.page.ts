87fa9477a4439e818f7ab3f4d89b0fdae564a55d
balaraju <balaraju@bijib.in>
2025-06-23 15:45:21 +0530

    slider close

--- src/app/layout/home/side-bar/side-bar.page.ts
+++ src/app/layout/home/side-bar/side-bar.page.ts
@@ -1,24 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonList, IonItem, IonLabel, IonButtons, IonButton, MenuController } from '@ionic/angular/standalone';
 
 @Component({
  selector: 'app-side-bar',
  templateUrl: './side-bar.page.html',
  styleUrls: ['./side-bar.page.scss'],
  standalone: true,
- imports: [IonLabel, IonItem, IonList, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+ imports: [IonLabel, IonItem, IonList, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, CommonModule, FormsModule]
 })
 export class SideBarPage {
+ private menuCtrl = inject(MenuController);
 
  tabsList: any = [
-  { id: 1, name: "Seach User", icon: "search" },
+  { id: 1, name: "Search User", icon: "search" },
   { id: 2, name: "User Requests", icon: "chatbubble-ellipses" },
   { id: 3, name: "Themes", icon: "color-wand" },
   { id: 4, name: "Settings", icon: "settings" }
- ]
- ngOnInit() {
+ ];
+
+ ngOnInit() { }
+
+ async closeSidebar() {
+  await this.menuCtrl.close("side-bar")
  }
- onTabClick(tab: any) { }
+
+ async onTabClick(tab: any) {
+  await this.menuCtrl.close("side-bar")
+ }
 }
