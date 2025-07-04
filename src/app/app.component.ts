7d9fc9215dfb9289eb8be0227a914840ab306e74
balaraju <balaraju@bijib.in>
2025-07-04 18:37:29 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { PushNotifications, Token, PushNotificationSchema, ActionPerformed } from '@capacitor/push-notifications';
 import { AlertService } from './utils/alert.service';
+import { Constants } from './utils/constants.service';
+import { LSService } from './utils/ls-service.service';
 
 @Component({
  selector: 'app-root',
@@ -25,10 +27,14 @@ export class AppComponent {
    }
   });
 
-  PushNotifications.addListener('registration', (token: Token) => {
+  PushNotifications.addListener('registration', async (token: Token) => {
    console.log('Push registration success, token: ' + token.value);
    AlertService.showAlert("Success", "Push registration success, token: " + JSON.stringify(token))
    // Send this token to your server for later use
+   if (token['value'] && token['value'].length > 0) {
+    Constants.FIREBASE_TOKEN = token['value'];
+    await LSService.setItem(token.value, Constants.LS_FIREBASE_TOKEN_KEY);
+   }
   });
 
   PushNotifications.addListener('registrationError', (error: any) => {
