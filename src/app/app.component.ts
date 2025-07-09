49052976248d66655cbe9d40facf77dd3f51742f
balaraju <balaraju@bijib.in>
2025-07-09 17:28:57 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core"
+import { Component, inject } from "@angular/core"
 import { Capacitor } from "@capacitor/core"
-import { IonApp, IonRouterOutlet } from "@ionic/angular/standalone"
+import { IonApp, IonRouterOutlet, ModalController } from "@ionic/angular/standalone"
 import { PushNotifications, Token, PushNotificationSchema, ActionPerformed } from "@capacitor/push-notifications"
 import { AlertService } from "./utils/alert.service"
 import { Constants } from "./utils/constants.service"
 import { LSService } from "./utils/ls-service.service"
+import { FriendChatPage } from "./layout/home/chat/friend-chat/friend-chat.page"
 
 @Component({
  selector: "app-root",
@@ -12,7 +13,7 @@ import { LSService } from "./utils/ls-service.service"
  imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-
+ private readonly modalCtrl = inject(ModalController)
  ngOnInit() {
   this.initializeApp()
  }
@@ -41,10 +42,35 @@ export class AppComponent {
 
    PushNotifications.addListener("pushNotificationActionPerformed", (notification: ActionPerformed) => {
     AlertService.showAlert("Notification", "Notification action performed: " + JSON.stringify(notification))
+    const data = notification.notification?.["data"] || ""
+    if (data) this.onClickNotification(data)
    })
   } else {
    console.log("Push Notifications not supported on web")
    // Optional: Fallback to web-based notifications using the Web Notifications API
   }
+ }
+
+ onClickNotification(data: any) {
+  const type = data["type"] || 0
+  switch (type) {
+   case 1:
+    break
+   case 2:
+    this.openFriendChatModal(data)
+    break
+   case 3:
+    break
+   case 4:
+    break
+  }
+ }
+ async openFriendChatModal(item: any) {
+  const data = { user_id: item["ref_id"], username: item["username"] }
+  const modal = await this.modalCtrl.create({
+   component: FriendChatPage,
+   componentProps: { friendData: data }
+  })
+  await modal.present()
  }
 }
