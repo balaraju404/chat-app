5822bc2f6f35bf405611aafdb0fe70dbd59b8c50
balaraju <balaraju@bijib.in>
2025-07-16 14:05:13 +0530

    environment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
  production: false,
- NODE_URL: 'https://chat-node-v0tt.onrender.com/'
+ NODE_URL: 'https://chat-node-v0tt.onrender.com/',
+ firebase: {
+  apiKey: "AIzaSyBgK06V5ZD-JbbRFWmv9sG_yTFKc-kGLus",
+  authDomain: "gbr-chat-app.firebaseapp.com",
+  projectId: "gbr-chat-app",
+  storageBucket: "gbr-chat-app.firebasestorage.app",
+  messagingSenderId: "620020366940",
+  appId: "1:620020366940:web:e4e72cc07720b0af293099",
+  measurementId: "G-FT0W4ZS239",
+  vapidKey: "BOLz47cBg0Lt8bYYJ4HEWKZOFJCF1MKRoeo_pORQhM5ftJ_KEGxyhSuPPD0J9y1l-MVzhb2nfYrgkYum_4r_zS0"
+ }
 }
